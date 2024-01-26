99466ce294d50d8d2313d6d06068af3f2fddc250
Willames Vital <willamescfvital@gmail.com>
2024-01-25 22:27:08 -0300

    Revert "Teste Pablin"
    This reverts commit 8c8e9b700532ff7bd17983f1bce3c1dae704fb65.

--- myshop.com/cypress/e2e/HomePageElements.cy.js
+++ myshop.com/cypress/e2e/HomePageElements.cy.js
@@ -1,8 +1,5 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://www.automationpractice.pl/index.php");
-    cy.visit(
-      "http://www.automationpractice.pl/index.php?id_category=8&controller=category"
-    );
-  });
-});
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://www.automationpractice.pl/index.php')
+  })
+})
